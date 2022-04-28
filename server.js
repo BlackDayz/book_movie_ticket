5e3b1bd58141ef01454fe8c3ed6a445e301673f2
Mittelblut9 <sauer.benedikt@outlook.de>
2022-04-28 18:09:08 +0200

    Inital Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const serverconfig = require('nconf');
+
+const app = express();
+app.use(express.static('public'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+serverconfig.argv().env().file({file: './src/json/config/config.json'});
+
+
+require('./server-init')(app, express);
+require('./server/route/mainroute')(app);
+
+app.listen(serverconfig.get('port'), serverconfig.get('domain'), () => {
+    console.log(`http://${
+        serverconfig.get('domain')
+    }:${serverconfig.get('port')} server started on ${
+        serverconfig.get('port')
+    }`);
+});
