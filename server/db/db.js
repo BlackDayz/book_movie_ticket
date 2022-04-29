0d633361b5a10fc57024d8ea131572b2ffdb34b7
Mittelblut9 <sauer.benedikt@outlook.de>
2022-04-29 18:08:04 +0200

    added database

---
+++ server/db/db.js
@@ -0,0 +1,42 @@
+const mysql = require('mysql');
+const dbconfig = require('./dbconfig.json');
+const nconf = require('nconf');
+
+class Database {
+  constructor() {
+    this.connection = mysql.createPool({
+      connectionLimit: 50,
+      host: dbconfig.host,
+      user: dbconfig.user,
+      password: dbconfig.password,
+      database: dbconfig.database,
+      debug: nconf.get('db_debug')
+    });
+
+    this.connection.getConnection((err, connection) => {
+      if (err) throw err;
+      console.log('-------Database connected successfully------');
+      connection.release();
+    });
+  }
+  query(sql, args) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(sql, args, (err, rows) => {
+        if (err) return reject(err);
+        resolve(rows);
+      })
+    })
+  }
+  close() {
+    return new Promise((resolve, reject) => {
+      this.connection.end(err => {
+        if (err) return reject(err);
+        resolve();
+      })
+    })
+  }
+}
+
+const database = new Database();
+
+module.exports = database;
